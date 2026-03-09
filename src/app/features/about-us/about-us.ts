e5c25639e224587b39e4bb89c3afdbc63a58709a
Yon Chando <von.chando@gmail.com>
2026-03-09 21:10:33 +0700

    Remove unused image assets

--- src/app/features/about-us/about-us.ts
+++ src/app/features/about-us/about-us.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {Slider} from "@/app/shared/components/slider/slider";
 import { Shade } from '@/app/shared/components/svg/shade/shade';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
     selector: 'app-about-us',
-    imports: [Slider, Shade],
+    imports: [Slider, Shade, NgOptimizedImage],
     templateUrl: './about-us.html',
     styleUrl: './about-us.css',
 })
